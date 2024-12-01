916eb6a2dc564fa2015d3b9953bb0191500f80f7
divyKS <divycodescompetes@gmail.com>
2024-12-01 12:41:47 +0530

    /api addded to url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://entnt-backend-dsaq.onrender.com";
+const BASE_URL = "https://entnt-backend-dsaq.onrender.com/api";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
